12daee2566aadcdabb4537fcb7ea0fbb01f89231
tpillow <jackofall55@gmail.com>
2025-11-15 22:33:10 -0500

    TileMapEntityCoordinator listen to entities

--- nodes/tile_map/tile_map_entity_coordinator.gd
+++ nodes/tile_map/tile_map_entity_coordinator.gd
@@ -7,11 +7,18 @@ class _EntityCell extends Object:
 
 const TILE_COORD_NULL: Vector2i = Vector2i.MIN
 
+# Tile size used to control coordinate conversions; based on this node's global position
 @export var tile_size: Vector2 = Vector2(8, 8)
+# Whether or not entities added/removed should be children of this node or not
 @export var entities_are_children: bool = true
+# Maximum number of entities per grid cell
 @export var max_entities_per_cell: int = 1
+# If true, entities must have a "tile_coord_changed(old, new)" signal, and when
+# triggered, this coordinator will move the entity
+@export var listen_to_entities: bool = true
 
 var _entity_cells: Dictionary[Vector2i, _EntityCell] = {}
+var _entity_tile_coord_changed_callbacks: Dictionary[Node2D, Callable] = {}
 
 func _ready() -> void:
 	if entities_are_children:
@@ -19,6 +26,7 @@ func _ready() -> void:
 			if child.tile_coord == TILE_COORD_NULL:
 				child.tile_coord = global_to_tile_coord(child.global_position)
 			_add_entity_cell_only(child)
+			_attach_entity_listener(child)
 
 func add_entity(entity: Node2D, tile_coord: Vector2i = TILE_COORD_NULL) -> void:
 	_add_entity_cell_only(entity, tile_coord)
@@ -27,8 +35,11 @@ func add_entity(entity: Node2D, tile_coord: Vector2i = TILE_COORD_NULL) -> void:
 		assert(entity not in get_children())
 		add_child(entity)
 		
-func move_entity(entity: Node2D, tile_coord: Vector2i) -> void:
-	_remove_from_cell(entity, entity.tile_coord)
+	_attach_entity_listener(entity)
+		
+func move_entity(entity: Node2D, tile_coord: Vector2i, from_coord: Vector2i = TILE_COORD_NULL) -> void:
+	var old_coord: Vector2i = entity.tile_coord if from_coord == TILE_COORD_NULL else from_coord
+	_remove_from_cell(entity, old_coord)
 	_add_to_cell(entity, tile_coord)
 	
 func remove_entity(entity: Node2D) -> void:
@@ -37,6 +48,8 @@ func remove_entity(entity: Node2D) -> void:
 	if entities_are_children:
 		assert(entity in get_children())
 		remove_child(entity)
+		
+	_detach_entity_listener(entity)
 		
 func tile_coord_to_global(tile_coord: Vector2i, centered: bool = true) -> Vector2:
 	var offset := tile_size / 2.0 if centered else Vector2.ZERO
@@ -52,6 +65,22 @@ func get_entities_at(tile_coord: Vector2i) -> Array[Node2D]:
 
 func num_entities_at(tile_coord: Vector2i) -> int:
 	return get_entities_at(tile_coord).size()
+		
+func _attach_entity_listener(entity: Node2D) -> void:
+	if not listen_to_entities:
+		return
+	assert(entity not in _entity_tile_coord_changed_callbacks)
+	var callback := func(old_coord: Vector2i, new_coord: Vector2i) -> void:
+		move_entity(entity, new_coord, old_coord)
+	_entity_tile_coord_changed_callbacks[entity] = callback
+	entity.tile_coord_changed.connect(callback)
+	
+func _detach_entity_listener(entity: Node2D) -> void:
+	if not listen_to_entities:
+		return
+	assert(entity in _entity_tile_coord_changed_callbacks)
+	entity.tile_coord_changed.disconnect(_entity_tile_coord_changed_callbacks[entity])
+	_entity_tile_coord_changed_callbacks.erase(entity)
 		
 func _add_entity_cell_only(entity: Node2D, tile_coord: Vector2i = TILE_COORD_NULL) -> void:
 	assert("tile_coord" in entity,
@@ -72,7 +101,8 @@ func _add_to_cell(entity: Node2D, tile_coord: Vector2i) -> void:
 	assert(cell.entities.size() < max_entities_per_cell,
 		"cannot add entity: cell maximum reached")
 	cell.entities.append(entity)
-	entity.tile_coord = tile_coord
+	if not listen_to_entities:
+		entity.tile_coord = tile_coord
 	
 func _remove_from_cell(entity: Node2D, tile_coord: Vector2i) -> void:
 	assert(tile_coord in _entity_cells)
