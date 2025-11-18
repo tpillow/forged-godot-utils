cfe15697ca44f9de8a98a0ca36a9917473e72e42
tpillow <jackofall55@gmail.com>
2025-11-17 19:28:40 -0500

    Refactor scene_manager to have UI canvas layers

--- scripts/scene_manager/scene_manager.gd
+++ scripts/scene_manager/scene_manager.gd
@@ -1,9 +1,12 @@
 class_name SceneManager
 extends Node
 
-var TRANS_INSTANT := SceneTransitionInstant.new()
-var TRANS_COLOR_FROM_CENTER := SceneTransitionColorFromCenter.new()
-var TRANS_FADE := SceneTransitionFade.new()
+var overlays_canvas_layer_num: int = 100
+var ui_canvas_layer_num: int = 50
+
+var trans_instant := SceneTransitionInstant.new()
+var trans_color_from_center := SceneTransitionColorFromCenter.new()
+var trans_fade := SceneTransitionFade.new()
 
 var _scenes_container: Node
 var _overlays_container: CanvasLayer
@@ -16,10 +19,10 @@ var top_scene: Node:
 	
 func _ready() -> void:
 	_scenes_container = Node.new()
-	get_parent().add_child.call_deferred(_scenes_container)
+	add_child(_scenes_container)
 	_overlays_container = CanvasLayer.new()
-	_overlays_container.layer = 1
-	get_parent().add_child.call_deferred(_overlays_container)
+	_overlays_container.layer = overlays_canvas_layer_num
+	add_child(_overlays_container)
 
 func _pop_all_scenes(free: bool = true) -> Array[Node]:
 	var ret: Array[Node] = []
@@ -46,14 +49,29 @@ func _enable_top_scene() -> void:
 	assert(top_scene)
 	top_scene.process_mode = Node.PROCESS_MODE_INHERIT
 	
+func _wrap_in_ui_canvas(node: Node) -> CanvasLayer:
+	var canvas := CanvasLayer.new()
+	canvas.layer = ui_canvas_layer_num
+	canvas.add_child(node)
+	return canvas
+	
 func replace_all(new_scene: Node, transition: SceneTransition) -> void:
 	transition.begin(self, SceneTransition.Type.REPLACE_ALL, new_scene)
-	
+
+func replace_all_with_ui(new_scene: Node, transition: SceneTransition) -> void:
+	replace_all(_wrap_in_ui_canvas(new_scene), transition)
+
 func replace(new_scene: Node, transition: SceneTransition) -> void:
 	transition.begin(self, SceneTransition.Type.REPLACE, new_scene)
-	
+
+func replace_with_ui(new_scene: Node, transition: SceneTransition) -> void:
+	replace(_wrap_in_ui_canvas(new_scene), transition)
+
 func push(new_scene: Node, transition: SceneTransition) -> void:
 	transition.begin(self, SceneTransition.Type.PUSH, new_scene)
-	
+
+func push_ui(new_scene: Node, transition: SceneTransition) -> void:
+	push(_wrap_in_ui_canvas(new_scene), transition)
+
 func pop(transition: SceneTransition) -> void:
 	transition.begin(self, SceneTransition.Type.POP, null)
