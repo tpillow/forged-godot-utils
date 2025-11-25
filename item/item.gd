ca970694b142264905e010c37f8aea361fb6a145
tpillow <jackofall55@gmail.com>
2025-11-25 14:56:48 -0500

    Re commit item icon change

--- item/item.gd
+++ item/item.gd
@@ -14,6 +14,12 @@ extends Resource
 		description = value
 		changed.emit()
 
+@export var icon: Texture2D:
+	get: return icon
+	set(value):
+		icon = value
+		changed.emit()
+
 @export var max_stack_count: int = 1:
 	get: return max_stack_count
 	set(value):
