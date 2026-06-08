e90e93453f64d5b85371dc6600dafec642e96822
Tony <73753540+tpillow@users.noreply.github.com>
2026-06-08 12:23:15 -0400

    Update prop_tween_trigger.gd

--- tween_utils/triggers/prop_tween_trigger.gd
+++ tween_utils/triggers/prop_tween_trigger.gd
@@ -3,12 +3,26 @@ extends TweenTrigger
 
 @export var prop_name: String
 @export var value: Variant
+@export var on_ready_eval_prop_trigger_mode := TweenTrigger.OnReadyTriggerMode.NONE
 
 var _last_value: Variant
 
 func _ready() -> void:
 	super()
 	_last_value = trigger_source.get(prop_name)
+
+    match on_ready_eval_prop_trigger_mode:
+        TweenTrigger.OnReadyTriggerMode.NONE: pass
+		TweenTrigger.OnReadyTriggerMode.TRIGGER:
+			assert(on_ready_trigger_mode == TweenTrigger.OnReadyTriggerMode.NONE, "base TweenTrigger already has ready set")
+			if _last_value != value: break
+			do_trigger.call_deferred()
+		TweenTrigger.OnReadyTriggerMode.TRIGGER_FORCE_COMPLETE:
+			assert(false, "unimplemented!")
+            assert(on_ready_trigger_mode == TweenTrigger.OnReadyTriggerMode.NONE, "base TweenTrigger already has ready set")
+			if _last_value != value: break
+			force_complete.call_deferred()
+        _: assert(false)
 	
 func _process(delta: float) -> void:
 	var cur_value = trigger_source.get(prop_name)
