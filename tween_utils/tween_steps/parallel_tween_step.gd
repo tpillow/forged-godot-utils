277389c70b442faa173e1736b5ea39ae25942af6
Tony <73753540+tpillow@users.noreply.github.com>
2026-06-08 11:26:36 -0400

    Update parallel_tween_step.gd

--- tween_utils/tween_steps/parallel_tween_step.gd
+++ tween_utils/tween_steps/parallel_tween_step.gd
@@ -2,6 +2,8 @@ class_name ParallelTweenStep
 extends TweenStep
 
 func apply_tween() -> void:
+    # TODO: not call parallel on first tween?
 	for ts in NodeUtil.find_all_children_of_type(self, TweenStep, false):
 		tween_anim.tween.parallel()
 		ts.apply_tween()
+    tween_anim.tween.tween_callback(completed.emit)
