5964219859faa6a366a976aeab171f750b11cfcf
Tony <73753540+tpillow@users.noreply.github.com>
2026-06-08 11:35:16 -0400

    Update prop_tween_step.gd

--- tween_utils/tween_steps/prop_tween_step.gd
+++ tween_utils/tween_steps/prop_tween_step.gd
@@ -4,19 +4,14 @@ extends TweenStep
 @export var prop_path := ""
 @export var easing: Tween.EaseType = Tween.EASE_OUT
 @export var trans: Tween.TransitionType = Tween.TRANS_EXPO
-@export var end_value: Variant
-@export var end_value_set_initial_prop := ""
-@export var end_value_initial_prop_offset: Variant
 @export var duration := 1.0
 
-func apply_tween() -> void:
-	var real_end_value = end_value
-	var cache_value = tween_anim.source.intial_node_prop_cache.get(end_value_set_initial_prop)
-	if cache_value != null:
-		real_end_value = cache_value
-		if end_value_initial_prop_offset != null:
-			real_end_value += end_value_initial_prop_offset
+@export var end_value: Variant
+@export var end_value_is_relative := false
 
+func apply_tween() -> void:
 	tween_anim.tween.set_ease(easing).set_trans(trans)
-	tween_anim.tween.tween_property(tween_anim.source.node, prop_path, real_end_value, duration)
+	var prop_tween := tween_anim.tween.tween_property(tween_anim.source.node, prop_path, end_value, duration)
+    if end_value_is_relative:
+        prop_tween.as_relative()
     tween_anim.tween.tween_callback(completed.emit)
