6297be75126b6cd724124a2e7af9da547c6a937b
Tony <73753540+tpillow@users.noreply.github.com>
2026-06-08 12:48:37 -0400

    simple_ui_tween_step.gd to single TO_OFF_SCREEN_DIR

--- tween_utils/tween_steps/simple_ui_tween_step.gd
+++ tween_utils/tween_steps/simple_ui_tween_step.gd
@@ -3,14 +3,16 @@ extends TweenStep
 
 enum UiTweenType {
 	TO_INITIAL_POS,
-	SLIDE_OUT_LEFT,
-	SLIDE_OUT_RIGHT,
+    TO_OFF_SCREEN_DIR,
 }
 
 @export var ui_tween_type: UiTweenType = UiTweenType.TO_INITIAL_POS
 @export var easing: Tween.EaseType = Tween.EASE_OUT
 @export var trans: Tween.TransitionType = Tween.TRANS_EXPO
 @export var duration := 1.0
+
+# TODO: make proper dir enum and use here
+@export var off_screen_dir := Vector2i.LEFT
 
 var source_view_size: Vector2:
     get: return tween_anim.source.node.get_viewport().get_visible_rect().size
@@ -28,14 +30,16 @@ func apply_tween() -> void:
 	match ui_tween_type:
 		UiTweenType.TO_INITIAL_POS:
 			tween_anim.tween.tween_property(tween_anim.source.node, "position", _initial_position, duration)
-		UiTweenType.SLIDE_OUT_LEFT:
+		UiTweenType.TO_OFF_SCREEN_DIR:
+			assert(off_screen_dir != Vector2i.ZERO)
+
             var pos: Vector2 = tween_anim.source.node.position
 			var size: Vector2 = tween_anim.source.node.size
-			var end_value := Vector2(-size.x, pos.y)
-			tween_anim.tween.tween_property(tween_anim.source.node, "position", end_value, duration)
-		UiTweenType.SLIDE_OUT_RIGHT:
-            var pos: Vector2 = tween_anim.source.node.position
-			var end_value := Vector2(source_view_size.x, pos.y)
+
+			var xp := -size.x if off_screen_dir.x < 0 else ( source_view_size.x if off_screen_dir.x > 0 else pos.x )
+			var yp := -size.y if off_screen_dir.y < 0 else ( source_view_size.y if off_screen_dir.y > 0 else pos.y )
+			var end_value := Vector2(xp, yp)
+
 			tween_anim.tween.tween_property(tween_anim.source.node, "position", end_value, duration)
 		_: assert(false)
 
